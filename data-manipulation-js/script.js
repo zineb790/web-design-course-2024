2a53dec02bb48b9dbf5cbf082c14674514394715
zineb790 <zineb90abdel12@gmail.com>
2024-05-27 17:27:24 -0400

    part2 Practical Math

--- data-manipulation-js/script.js
+++ data-manipulation-js/script.js
@@ -65,3 +65,67 @@ console.log(`dividing Result By Fourth number: ${resultMultipliedByThird} /${n4}
 // Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
 const allUnderOrEqual25 = n1 <= 25 && n2 <= 25 && n3 <= 25 && n4 <= 25;
 console.log(` are all numbers Under Or Equal 25 :${allUnderOrEqual25}`);
+console.log("*********************************");
+// You are planning a cross-country road trip!
+// The distance of the trip, in total, is 1,500 miles.
+// Your car’s fuel efficiency is as follows:
+// At 55 miles per hour, you get 30 miles per gallon.
+// At 60 miles per hour, you get 28 miles per gallon.
+// At 75 miles per hour, you get 23 miles per gallon.
+// You have a fuel budget of $175.
+// The average cost of fuel is $3 per gallon.
+// Set up a program to answer the following questions:
+// How many gallons of fuel will you need for the entire trip?
+// Will your budget be enough to cover the fuel expense?
+// How long will the trip take, in hours?
+// Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
+// Log the results of your calculations using string concatenation or template literals.
+const totalDistance = 1500; 
+const fuelBudget = 175; 
+const fuelCostPerGallon = 3; 
+const milesPerHour = [55, 60, 75];
+const fuelEfficiency = {
+    55: 30,
+    60: 28,
+    75: 23
+}
+
+//calculating gallons needed for each speed
+//at 55 MPH
+const gallonsForTrip55 = totalDistance / fuelEfficiency[55];
+console.log(`gallons needed for the trip at 55 mpr is :${gallonsForTrip55}`);
+//at 50 MPH
+const gallonsForTrip60 = totalDistance / fuelEfficiency[60];
+console.log(`gallons needed for the trip at 60 mpr is :${gallonsForTrip60}`);
+//at 75 MPH
+const gallonsForTrip75 = totalDistance / fuelEfficiency[75];
+console.log(`gallons needed for the trip at 55 mpr is :${gallonsForTrip75}`);
+//average gallons needed for the trip
+const averageGallonsNeededForTrip = (gallonsForTrip55 + gallonsForTrip60 + gallonsForTrip75) / 3;
+console.log(`average Gallons Needed For Trip is :${averageGallonsNeededForTrip}`);
+
+// Will your budget be enough to cover the fuel expense?
+
+if (averageGallonsNeededForTrip * fuelCostPerGallon <= fuelBudget) {
+    console.log(`yes my fuelbadget is enough for the trip`)
+} else {
+     console.log(`no my fuelbadget is not enough for the trip`)
+}
+// calculating how many hours will the trip take at each speed
+// at 55 mph
+const totalTripHoursAt55 = totalDistance / 55;
+console.log(`total Trip Hours At 55 mpr is :${totalTripHoursAt55}`);
+//at 60 mph
+const totalTripHoursAt60 = totalDistance / 60;
+console.log(`total Trip Hours At 60 mpr is :${totalTripHoursAt60}`);
+//at 75 mph
+const totalTripHoursAt75 = totalDistance / 75;
+console.log(`total Trip Hours At 75 mpr is :${totalTripHoursAt75}`);
+
+
+
+
+
+
+
+
